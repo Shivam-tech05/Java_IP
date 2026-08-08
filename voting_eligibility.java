651495584d12f6bd54973d180fca6077680edf62
Shivam-tech05 <shivamjaiswal05052006@gmail.com>
2026-08-08 12:17:42 +0530

    Voting Criteria

--- voting_eligibility.java
+++ voting_eligibility.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class voting_eligibility {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Age:");
+        int age = sc.nextInt();
+        if(age >= 18){
+            System.out.println("You are eligible to vote.");
+        }
+        else{
+            System.out.println("You are not eligible to vote.");
+        }
+    }
 }
