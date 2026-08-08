aa013f20f94cdcb85439b988891d6ec27d9c5857
Shivam-tech05 <shivamjaiswal05052006@gmail.com>
2026-08-08 13:21:47 +0530

    Bill calculation

--- Bill_calc.java
+++ Bill_calc.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
-public class bill_calc {
+public class Bill_calc {
     public static void main(String[] args){
         try(Scanner sc = new Scanner(System.in)){
             System.out.print("Enter the price of the item:");
             double price = sc.nextDouble();
-            System.out.print("Enter ")
+            System.out.print("Enter the quantity of the item:");
+            int quantity = sc.nextInt();
+            double total = price * quantity;
+            System.out.println("The total bill is: " + total);
+            sc.close();
         }
     }
 }
