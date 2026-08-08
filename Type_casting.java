a11e5d9b0456a0766d6e87e509f64cac1fa78a86
Shivam-tech05 <shivamjaiswal05052006@gmail.com>
2026-08-08 11:53:34 +0530

    Updated Type_casting

--- Type_casting.java
+++ Type_casting.java
@@ -2,13 +2,21 @@ public class Type_casting{
     public static void main(String[] args){
         // Example of type casting
         int a = 12;
-        double b = a;   // Implicit casting (int to double)
+        double b = a;   // Widening casting (int to double)
         System.out.println("Value of a: " + a);
         System.out.println("Value of b: " + b);
-        //Explicit casting (double to int)
+        //Narrowing casting (double to int)
         double c = 9.78;
-        int d = (int) c; // Explicit casting
+        int d = (int) c; // Narrowing casting
         System.out.println("Value of c: " + c);
         System.out.println("Value of d: " + d); 
+        // ceiling and floor value of double
+        double e = 5.75;
+        System.out.println("Ceiling value is:" + (int)Math.ceil(e));
+        System.out.println("Floor value is:" + (int)Math.floor(e));
     }
+    // Type casting order 
+    //widening type casting: byte -> short -> char -> int -> long -> float -> double
+    //narrowing type casting: double -> float -> long -> int -> char -> short -> byte
+
 }
